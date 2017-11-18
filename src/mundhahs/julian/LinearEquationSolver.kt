2fcb2df6fcccd47e788ef2de1f57dba2b5a1b9b3
Julian Mundhahs <julian.mundhahs@gmail.com>
2017-11-18 13:08:16 +0100

    Extract the ability to solve a linear system to a interface Extracting the function to solve these system to a interface allows classes to implement the LinearEquationSystemSolver. This allows for a better separation of responsibility

---
+++ src/mundhahs/julian/LinearEquationSolver.kt
@@ -0,0 +1,10 @@
+package mundhahs.julian
+
+/**
+ * Created by Julian Mundhahs on 18.11.2017.
+ */
+interface LinearEquationSolver {
+
+    public fun solve(system: Gleichungssystem): Gleichungssystem
+
+}
