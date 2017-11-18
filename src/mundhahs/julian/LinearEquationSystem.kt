483016e756de0ff4e0df0881f5be2918cf0df979
Julian Mundhahs <julian.mundhahs@gmail.com>
2017-11-18 16:03:16 +0100

    All eqautions will be fill to the same length to make further processing easier

--- src/mundhahs/julian/LinearEquationSystem.kt
+++ src/mundhahs/julian/LinearEquationSystem.kt
@@ -8,7 +8,23 @@ class LinearEquationSystem(vararg gleichungen: Gleichung) {
     var gleichungen: Array<Gleichung>
 
     init {
+        check(gleichungen.size != 0)
+
+        val max: Int = gleichungen.maxBy { it.faktoren.size }!!.faktoren.size
+
         this.gleichungen = gleichungen.toList().toTypedArray()
+
+        this.gleichungen = Array(gleichungen.size, { i -> this.gleichungen[i].fillTo(max)})
+    }
+
+    fun getEquationsAmount(): Int
+    {
+        return gleichungen.size
+    }
+
+    fun getEquationsLength(): Int
+    {
+        return gleichungen[0].faktoren.size
     }
 
 }
