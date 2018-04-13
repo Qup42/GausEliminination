b83656b020751b6fd864720b791a36c9ae9015aa
Julian Mundhahs <julian.mundhahs@gmail.com>
2018-04-13 20:23:02 +0200

    Add HTMl and javascript for the webpage

---
+++ javascript.js
@@ -0,0 +1,36 @@
+let numEquations = 4;
+let numUnknowns = 4;
+
+function setupTable() {
+    
+}
+
+function addUnknown() {
+
+}
+
+function removeUnknown() {
+
+}
+
+function addEqution() {
+
+}
+
+function removeEqution() {
+
+}
+
+
+$("#numUnknows").change(function (event) {
+
+});
+$("#numEquations").change(function (event) {
+
+});
+
+$("#calculate").click(function (event) {
+    alert("clicked");
+});
+//console.log(GausEliminination.mundhahs.julian.main());
+$(document).ready(() => GausEliminination.mundhahs.julian.setupListeners());
