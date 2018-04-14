74192dd26eb6a9cab480f7e34f9a745ec4129911
Julian Mundhahs <julian.mundhahs@gmail.com>
2018-04-14 18:28:09 +0200

    Add extension function (String -> Bruch), also process the results of the default system and display it

--- src/mundhahs/julian/Main.kt
+++ src/mundhahs/julian/Main.kt
@@ -1,5 +1,11 @@
 package mundhahs.julian
 
+import kotlinx.html.dom.append
+import kotlinx.html.js.hr
+import kotlinx.html.p
+import kotlin.browser.document
+import kotlin.browser.window
+
 fun main(args: Array<String>) {
     Main.main(arrayOf())
 }
@@ -21,10 +27,10 @@ object Main {
         1 freier parameter
          */
         val a = Gleichung(2.br, arrayOf(1.br, 1.br, 5.br, 3.br, 3.br))
-        val b = Gleichung(3.br, arrayOf(2.br,3.br, (-1).br, 7.br, 3.br))
-        val c = Gleichung(3.br, arrayOf(2.br,3.br, 2.br, 12.br, 3.br))
-        val d = Gleichung(1.br, arrayOf(4.br,2.br, 0.br, 69.br, 3.br))
-        val lgs = LinearEquationSystem(a,b,c, d)
+        val b = Gleichung(3.br, arrayOf(2.br, 3.br, (-1).br, 7.br, 3.br))
+        val c = Gleichung(3.br, arrayOf(2.br, 3.br, 2.br, 12.br, 3.br))
+        val d = Gleichung(1.br, arrayOf(4.br, 2.br, 0.br, 69.br, 3.br))
+        val lgs = LinearEquationSystem(a, b, c, d)
 
         /*val a = Gleichung(2.br, arrayOf(1.br, 1.br, 5.br, 3.br))
         val b = Gleichung(3.br, arrayOf(2.br,3.br, (-1).br, 7.br))
@@ -39,7 +45,6 @@ object Main {
         val lgs = LinearEquationSystem(a,b,c,d)*/
 
 
-
         /*
         val a = Gleichung(3.br, arrayOf(1.br, 1.br))
         val b = Gleichung(1.br, arrayOf(1.br, 0.br))
@@ -49,11 +54,33 @@ object Main {
 
         val solver: LinearEquationSolver = GausElminiationSolver()
 
-        solver.solve(lgs)
+        val solved = solver.solve(lgs)
 
-        print(lgs)
+        val values = (solver as GausElminiationSolver).harvest1(solved)
+
+        //console.log(values)
+        console.log(values.keys.joinToString(", "))
+        window.onload = {
+            values.forEach { console.log(it.value.getResult()) }
+            val resultDiv = document.getElementById("result")
+            values.forEach {
+                resultDiv!!.append {
+                    p {
+                        +it.value.getResult()
+                    }
+                }
+            }
+            resultDiv!!.append { hr { } }
+        }
     }
 }
 
 public val Int.br: Bruch
-    get() {return Bruch(this, 1)}
+    get() {
+        return Bruch(this, 1)
+    }
+
+public val String.toBruch: Bruch
+    get() {
+        return Bruch(this.toInt(), 1)
+    }
