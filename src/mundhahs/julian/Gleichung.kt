7196064ebea84df266111bf9e3201547f6acf265
Qup42 <julian.mundhahs@gmail.com>
2018-04-15 13:49:49 +0200

    Provide additional meaningful operator functions and overwrite equals+hashcode

--- src/mundhahs/julian/Gleichung.kt
+++ src/mundhahs/julian/Gleichung.kt
@@ -8,6 +8,8 @@ class Gleichung(val ergebnis: Bruch, val faktoren: Array<Bruch>) {
         return "$toString] = $ergebnis"
     }
 
+    operator fun unaryMinus(): Gleichung = Gleichung(-ergebnis, Array<Bruch>(faktoren.size, {i -> -faktoren.get(i)}))
+
     operator fun plus(gleichung: Gleichung): Gleichung
     {
         val neueFaktoren: Array<Bruch> = Array<Bruch>(maxOf(faktoren.size, gleichung.faktoren.size),{i -> (faktoren.getOrElse(i, {0.br}))+gleichung.faktoren.getOrElse(i, {0.br})})
@@ -15,21 +17,39 @@ class Gleichung(val ergebnis: Bruch, val faktoren: Array<Bruch>) {
         return Gleichung(ergebnis+gleichung.ergebnis, neueFaktoren)
     }
 
-    operator fun div(bruch: Bruch): Gleichung
-    {
-        return Gleichung(ergebnis/bruch, Array<Bruch>(faktoren.size, {i -> (faktoren[i]/bruch)}))
-    }
-
     operator fun minus(gleichung: Gleichung): Gleichung
     {
         return this+-gleichung
     }
 
-    operator fun unaryMinus(): Gleichung = Gleichung(-ergebnis, Array<Bruch>(faktoren.size, {i -> -faktoren.get(i)}))
-
     operator fun times(faktor: Bruch): Gleichung
     {
         return Gleichung(ergebnis*faktor, Array<Bruch>(faktoren.size, {i -> (faktoren[i]*faktor)}))
+    }
+
+    operator fun div(bruch: Bruch): Gleichung
+    {
+        return Gleichung(ergebnis/bruch, Array<Bruch>(faktoren.size, {i -> (faktoren[i]/bruch)}))
+    }
+
+    operator fun contains(bruch: Bruch) = faktoren.contains(bruch)
+
+    operator fun get(i: Int) = faktoren[i]
+
+    operator fun set(i: Int, bruch: Bruch)
+    {
+        faktoren[i] = bruch
+    }
+
+    override operator fun equals(other: Any?): Boolean {
+        return (other is Gleichung && other.ergebnis==this.ergebnis && other.faktoren.contentEquals(this.faktoren))
+    }
+
+    override fun hashCode(): Int
+    {
+        var result = ergebnis.hashCode()
+        result = 31 * result + faktoren.hashCode()
+        return result
     }
 
     fun fillTo(size: Int): Gleichung
