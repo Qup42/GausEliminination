390f8dfa24d22a8a1c96c0c008a2bfbd0a611ca6
Qup42 <julian.mundhahs@gmail.com>
2018-04-15 21:07:24 +0200

    Add a service worker that caches the site

--- javascript.js
+++ javascript.js
@@ -8,3 +8,15 @@ $("#clear_results").click(function () {
 
 $(document).ready(() => GausEliminination.mundhahs.julian.setupListeners());
 GausEliminination.mundhahs.julian.setupTable();
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('/serviceWorker.js').then(function(registration) {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function(err) {
+            // registration failed :(
+            alert('ServiceWorker registration failed: ', err);
+        });
+    });
+}
