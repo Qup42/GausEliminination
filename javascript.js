e55be415930f086a7979f6d763862cbf69294518
Qup42 <julian.mundhahs@gmail.com>
2018-04-15 19:53:24 +0200

    Added a button the clear the result history

--- javascript.js
+++ javascript.js
@@ -2,6 +2,9 @@ $("#calculate").click(function () {
     GausEliminination.mundhahs.julian.fillEmptyInputsWithZero();
     GausEliminination.mundhahs.julian.calculate();
 });
+$("#clear_results").click(function () {
+    $("#result").empty();
+});
 
 $(document).ready(() => GausEliminination.mundhahs.julian.setupListeners());
 GausEliminination.mundhahs.julian.setupTable();
