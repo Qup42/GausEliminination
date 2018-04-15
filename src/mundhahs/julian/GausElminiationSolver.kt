Qup42/GausEliminination

commit aa0dccb391b76e819487c8db8e849cfd2526c67a

findNotZeroRow now returns -1 instead of null if now row is found

src/mundhahs/julian/GausElminiationSolver.kt
@@ -13,9 +13,9 @@ class GausElminiationSolver : LinearEquationSolver {
 
         for (spalte in 0 until system.getEquationsLength()) {
             //eigentlich sollten die spalten in diesem fall lieber getauscht werden
-            val notZeroRowIndex: Int? = findNotZeroRow(system, spalte, sprungStellen)
+            val notZeroRowIndex: Int = findNotZeroRow(system, spalte, sprungStellen)
 
-            if (notZeroRowIndex == null) {
+            if (notZeroRowIndex == -1) {
                 freieParameter++
                 continue
             } else {
@@ -52,9 +52,8 @@ class GausElminiationSolver : LinearEquationSolver {
         gleichungen[index1] = gleichungen[index2].also { gleichungen[index2] = gleichungen[index1] }
     }
 
-    private fun findNotZeroRow(system: LinearEquationSystem, spalte: Int, zeile: Int): Int? {
-
-        return (zeile until system.getEquationsAmount()).firstOrNull { system[it, spalte].zähler != 0 }
+    private fun findNotZeroRow(system: LinearEquationSystem, spalte: Int, zeile: Int): Int {
+        return (zeile until system.getEquationsAmount()).indexOfFirst { system[it, spalte].zähler != 0 }
     }
 
     private fun checkApparentDeterminationOfEqautionSystem(gleichungen: Array<Gleichung>) {

commit c701270576c40addd81c10961170dd4b38224e07

Fix error introduced in aa0dccb391b76e819487c8db8e849cfd2526c67a By changing findNotZeroRow's search function from return firstOrNull to indexOfFirst the result would be zeile smaller then wanted since the first zeile elements weren't queried. This is fixed by adding zeile back to the result in the end.

src/mundhahs/julian/GausElminiationSolver.kt
@@ -53,7 +53,10 @@ class GausElminiationSolver : LinearEquationSolver {
     }
 
     private fun findNotZeroRow(system: LinearEquationSystem, spalte: Int, zeile: Int): Int {
-        return (zeile until system.getEquationsAmount()).indexOfFirst { system[it, spalte].zähler != 0 }
+        //we're cutting of the first zeile elements and only return the index of the match.
+        //zeile is therefore added back when returning to get the total and not the position relative to the start of the search
+        val index = (zeile until system.getEquationsAmount()).indexOfFirst { system[it, spalte].zähler != 0 }
+        return index+zeile
     }
 
     private fun checkApparentDeterminationOfEqautionSystem(gleichungen: Array<Gleichung>) {