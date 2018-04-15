856e17306ce29d3222f3a0b35f87e97a2c03e50c
Qup42 <julian.mundhahs@gmail.com>
2018-04-15 14:00:24 +0200

    Change function oder in source code for Bruch.kt

--- src/mundhahs/julian/Bruch.kt
+++ src/mundhahs/julian/Bruch.kt
@@ -3,36 +3,6 @@ package mundhahs.julian
 data class Bruch(val zähler: Int, val nenner: Int) {
     //TODO: Konstruktor kürzen lassen
 
-    operator fun times(bruch: Bruch): Bruch {
-        return kürzen(zähler * bruch.zähler, nenner * bruch.nenner)
-    }
-
-    operator fun div(bruch: Bruch): Bruch {
-        return this*Bruch(bruch.nenner, bruch.zähler)
-    }
-
-    operator fun plus(bruch: Bruch): Bruch {
-        return when {
-            zähler==0 -> bruch
-            bruch.zähler==0 -> this
-            else -> kürzen(zähler*bruch.nenner+bruch.zähler*nenner, nenner*bruch.nenner)
-        }
-    }
-
-    override fun equals(other: Any?): Boolean {
-        if(other is Int)
-        {
-            return (nenner == 1) && (zähler == other)
-        }
-        else if(other is Bruch)
-        {
-            return (nenner == other.nenner) && (zähler == other.zähler)
-        }
-        return super.equals(other)
-    }
-
-    operator fun unaryMinus() = Bruch(-zähler, nenner)
-
     private fun gcd(int1: Int, int2: Int): Int {
         var a = int1
         var b = int2
@@ -52,6 +22,29 @@ data class Bruch(val zähler: Int, val nenner: Int) {
     private fun kürzen(zähler: Int, nenner: Int): Bruch {
         val gcd = gcd(zähler, nenner)
         return Bruch(zähler / gcd, nenner / gcd)
+    }
+
+    operator fun unaryMinus() = Bruch(-zähler, nenner)
+
+    operator fun plus(bruch: Bruch): Bruch {
+        return when {
+            zähler == 0 -> bruch
+            bruch.zähler == 0 -> this
+            else -> kürzen(zähler * bruch.nenner + bruch.zähler * nenner, nenner * bruch.nenner)
+        }
+    }
+
+    operator fun times(bruch: Bruch): Bruch = kürzen(zähler * bruch.zähler, nenner * bruch.nenner)
+
+    operator fun div(bruch: Bruch): Bruch = this * Bruch(bruch.nenner, bruch.zähler)
+
+    override operator fun equals(other: Any?): Boolean {
+        if (other is Int) {
+            return (nenner == 1) && (zähler == other)
+        } else if (other is Bruch) {
+            return (nenner == other.nenner) && (zähler == other.zähler)
+        }
+        return super.equals(other)
     }
 
     override fun hashCode(): Int {
