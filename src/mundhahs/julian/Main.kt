eaea53faee6710acf9c61bf227748879b25cc640
Qup42 <julian.mundhahs@gmail.com>
2018-04-15 19:45:35 +0200

    Extract the auto-run example to a separate function

--- src/mundhahs/julian/Main.kt
+++ src/mundhahs/julian/Main.kt
@@ -7,11 +7,20 @@ import kotlin.browser.document
 import kotlin.browser.window
 
 fun main(args: Array<String>) {
+    calculateExampleSystem()
+}
+
+fun calculateExampleSystem() {
     val a = Gleichung(2.br, arrayOf(1.br, 1.br, 5.br, 3.br, 3.br))
     val b = Gleichung(3.br, arrayOf(2.br, 3.br, (-1).br, 7.br, 3.br))
     val c = Gleichung(3.br, arrayOf(2.br, 3.br, 2.br, 12.br, 3.br))
     val d = Gleichung(1.br, arrayOf(4.br, 2.br, 0.br, 69.br, 3.br))
     val lgs = LinearEquationSystem(a, b, c, d)
+    /*val a = Gleichung(2.br, arrayOf(1.br, 0.br))
+    val b = Gleichung(4.br, arrayOf(2.br, 0.br))
+    val c = Gleichung(6.br, arrayOf(3.br, 0.br))
+    val d = Gleichung(5.br, arrayOf(0.br, 2.br))
+    val lgs = LinearEquationSystem(a,b,c,d)*/
 
     val solver: LinearEquationSolver = GausElminiationSolver()
 
