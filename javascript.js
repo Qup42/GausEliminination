0356e70b12582139e16032682bd98e78734e5b52
Qup42 <julian.mundhahs@gmail.com>
2018-04-21 22:08:10 +0200

    Javascript code cleanup and refactoring

--- javascript.js
+++ javascript.js
@@ -1,39 +1,9 @@
+let deferredPrompt;
+
 $("#calculate").click(function () {
     GausEliminination.mundhahs.julian.Werkzeug.fillEmptyInputsWithZero();
     GausEliminination.mundhahs.julian.Werkzeug.calculate();
-});
-$("#clear_results").click(function () {
-    $("#result").empty();
-});
 
-$(document).ready(() => GausEliminination.mundhahs.julian.Werkzeug.setupListeners());
-GausEliminination.mundhahs.julian.Werkzeug.setupTable();
-
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-        navigator.serviceWorker.register('/serviceWorker.js').then(function(registration) {
-            // Registration was successful
-            console.log('ServiceWorker registration successful with scope: ', registration.scope);
-        }, function(err) {
-            // registration failed :(
-            alert('ServiceWorker registration failed: ', err);
-        });
-    });
-}
-
-var deferredPrompt;
-
-window.addEventListener('beforeinstallprompt', function(e) {
-    console.log('beforeinstallprompt Event fired');
-    e.preventDefault();
-
-    // Stash the event so it can be triggered later.
-    deferredPrompt = e;
-
-    return false;
-});
-
-$("#calculate").click(function() {
     if(deferredPrompt !== undefined) {
         // The user has had a positive interaction with our app and Chrome
         // has tried to prompt previously, so let's show the prompt.
@@ -55,4 +25,32 @@ $("#calculate").click(function() {
             deferredPrompt = null;
         });
     }
+});
+$("#clear_results").click(function () {
+    $("#result").empty();
+});
+
+$(document).ready(() => GausEliminination.mundhahs.julian.Werkzeug.setupListeners());
+GausEliminination.mundhahs.julian.Werkzeug.setupTable();
+
+/*if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('./serviceWorker.js').then(function(registration) {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function(err) {
+            // registration failed :(
+            alert('ServiceWorker registration failed: ', err);
+        });
+    });
+}*/
+
+window.addEventListener('beforeinstallprompt', function(e) {
+    console.log('beforeinstallprompt Event fired');
+    e.preventDefault();
+
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+
+    return false;
 });
