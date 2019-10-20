1a402ab16eadfb4668ebd57c0cd83cdbad71bcc5
Qup42 <julian.mundhahs@gmail.com>
2019-10-20 16:58:07 +0200

    Increment Version-Tag

--- serviceWorker.js
+++ serviceWorker.js
@@ -14,7 +14,7 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const version = 18;
+const version = 19;
 const PRECACHE = `precache-v${version}`;
 const RUNTIME = 'runtime';
 
